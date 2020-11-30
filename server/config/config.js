3cf05d56fc7246546142113de5621ff7cfd7620f
Luis <jeshuagerald@hotmail.com>
2020-11-30 17:08:00 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://jeshuagerald:PdR0mFiR58LujORC@cluster0.rqnev.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI; //'mongodb+srv://jeshuagerald:PdR0mFiR58LujORC@cluster0.rqnev.mongodb.net/cafe';
 }
 
 process.env.URLDB = urlDB;
